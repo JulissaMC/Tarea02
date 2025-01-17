86a828a34957ebd055e8eb7e2069b173a0e3bb27
Felix Romero <ferofern@espol.edu.ec>
2025-01-17 12:18:26 -0500

    aaaa
    aaaa

--- tarea02patronesnotificaciones/tarea02patrones/src/main/java/com/mycompany/tarea02patrones/Notificacion.java
+++ tarea02patronesnotificaciones/tarea02patrones/src/main/java/com/mycompany/tarea02patrones/Notificacion.java
@@ -1,6 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
 package com.mycompany.tarea02patrones;
 
-
-public interface Notificacion {
-    public void enviarNotificacion(String destinatario, String mensaje);
+/**
+ *
+ * @author Ayman El Salous Mnz
+ */
+public interface notificacion {
+    public void enviarNotificacion(Cliente cliente, Reserva reserva){}
 }
